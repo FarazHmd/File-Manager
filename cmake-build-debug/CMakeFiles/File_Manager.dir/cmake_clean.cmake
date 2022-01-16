331ad1a9ac553b0cc0ae8c7ffa3eef9260152dc1
Faraz.Honarmand <89371191+FarazHmd@users.noreply.github.com>
2022-01-16 16:00:34 +0330

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/File_Manager.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../Build/File_Manager.exe"
+  "../Build/File_Manager.exe.manifest"
+  "../Build/File_Manager.pdb"
+  "CMakeFiles/File_Manager.dir/main.c.obj"
+  "libFile_Manager.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/File_Manager.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
